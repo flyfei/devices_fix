a1511c264a624b573b93e0240d365e017a4fa27a
flyfei <zhaotengfei9@gmail.com>
2017-03-15 16:03:38 +0800

    init

---
+++ app/src/main/java/com/tovi/devices/MainActivity.java
@@ -0,0 +1,114 @@
+package com.tovi.devices;
+
+import android.content.Context;
+import android.net.wifi.WifiManager;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView resText;
+    static final String key = "sys.rw.Deviceindex";
+
+    private String initMAC() {
+        WifiManager localWifiManager = (WifiManager) getSystemService("wifi");
+        if (localWifiManager.getConnectionInfo().getMacAddress() != null) {
+            return localWifiManager.getConnectionInfo().getMacAddress();
+        }
+        return null;
+
+    }
+
+    private int readIndex() {
+        String index = PropertyUtil.read(key);
+        int in;
+        try {
+            in = Integer.parseInt(index);
+        } catch (NumberFormatException e) {
+            return 0;
+        }
+        return in;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        resText = (TextView) findViewById(R.id.textView2);
+
+        findViewById(R.id.btn_refresh).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                TelephonyManager telepManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+                String imei = telepManager.getDeviceId();
+                String andId = Settings.Secure.getString(getContentResolver(),
+                        "android_id");
+                String mac = initMAC();
+
+                String imsi = telepManager.getSubscriberId();
+                String ss = telepManager.getSimSerialNumber();
+                String brand = android.os.Build.BRAND;
+                String model = android.os.Build.MODEL;
+                String text = String.format("%s,%s,%s,%s,%s,%s,%s,---%s", imei,
+                        andId, mac, imsi, ss, brand, model, String.valueOf(readIndex()));
+
+                resText.setText(text);
+                Log.d("hook", text);
+
+//				try {
+//					Class<?> clz = Class.forName("android.provider.Settings$Secure");
+//					Method methodGetAndroidId;
+//					try {
+//						methodGetAndroidId = clz.getMethod("getString",
+//								ContentResolver.class, String.class);
+//					} catch (NoSuchMethodException e) {
+//						methodGetAndroidId = null;
+//					}
+//					if(methodGetAndroidId!=null){
+//						Object obj = methodGetAndroidId.invoke(null, getContentResolver(),"android_id");
+//						String ssd = String.valueOf(obj);
+//						Log.d("dddd", ssd);
+//					}
+//				} catch (ClassNotFoundException e) {
+//					// TODO Auto-generated catch block
+//					e.printStackTrace();
+//				} catch (IllegalArgumentException e) {
+//					// TODO Auto-generated catch block
+//					e.printStackTrace();
+//				} catch (IllegalAccessException e) {
+//					// TODO Auto-generated catch block
+//					e.printStackTrace();
+//				} catch (InvocationTargetException e) {
+//					// TODO Auto-generated catch block
+//					e.printStackTrace();
+//				}
+            }
+        });
+
+        findViewById(R.id.btn_addindex).setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View v) {
+                int in = readIndex();
+                Log.d("devices_fix", "read index  " + in);
+                in++;
+                if (in >= Main.deviceIds.length) {
+                    in = 0; // reset
+                }
+                Log.d("devices_fix", "change index to " + in);
+                PropertyUtil.set(key, String.valueOf(in));
+
+                Log.d("devices_fix", "check index " + PropertyUtil.read(key));
+            }
+        });
+    }
+
+}
